229db9054de5682a2a0c4eb6fe23d00aa9b7d83c
mou-kexin <2584857586@qq.com>
2025-11-26 21:16:18 +0800

    博客系统代码

--- src/main/java/com/bite/springblogdemo/SpringBlogDemoApplication.java
+++ src/main/java/com/bite/springblogdemo/SpringBlogDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBlogDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBlogDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogDemoApplication.class, args);
+    }
 
 }
--- src/test/java/com/bite/springblogdemo/SpringBlogDemoApplicationTests.java
+++ src/test/java/com/bite/springblogdemo/SpringBlogDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBlogDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
