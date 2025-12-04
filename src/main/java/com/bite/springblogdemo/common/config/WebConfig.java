43497f9757a8dd168dd8338b517d00e1125dfde8
mou-kexin <2584857586@qq.com>
2025-12-04 22:19:13 +0800

    博客系统代码

--- src/main/java/com/bite/springblogdemo/common/config/WebConfig.java
+++ src/main/java/com/bite/springblogdemo/common/config/WebConfig.java
@@ -3,10 +3,11 @@ package com.bite.springblogdemo.common.config;
 import com.bite.springblogdemo.common.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configurable
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
--- src/main/java/com/bite/springblogdemo/common/interceptor/LoginInterceptor.java
+++ src/main/java/com/bite/springblogdemo/common/interceptor/LoginInterceptor.java
@@ -6,9 +6,11 @@ import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Slf4j
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
