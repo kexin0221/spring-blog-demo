6169d065c4c8c7899db26d318b0f4ec2069cf656
mou-kexin <2584857586@qq.com>
2025-12-05 11:50:19 +0800

    博客系统代码

--- src/main/java/com/bite/springblogdemo/common/config/WebConfig.java
+++ src/main/java/com/bite/springblogdemo/common/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.bite.springblogdemo.common.config;
 
 import com.bite.springblogdemo.common.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
